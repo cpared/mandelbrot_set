5a87fb475b8d03733baf6850f84feede4c2e055a
Cristian <capared91@gmail.com>
2020-08-03 19:03:48 -0300

    fix

--- zoom.js
+++ zoom.js
@@ -1,18 +1,14 @@
-const MAX_ITER = 100;
+const MAX_ITER = 50;
 var width = window.innerWidth;
 var height = window.innerHeight;
 
-var zoomx = 1;
-var zoomy = 1;
-var zoom = 1;
+var ratio = 1;
 
-var ratio = width / height;
+var zoom = 2;
 
 var div = document.getElementById('div');
 var x1 = 0;
 var y1 = 0;
-var x2 = width;
-var y2 = height;
 
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
@@ -20,44 +16,50 @@ var ctx = canvas.getContext('2d');
 ctx.canvas.width = width;
 ctx.canvas.height = height;
 
+var scale = Math.min(canvas.width / 600, canvas.height / 600);
+
 
 function draw(){
-
-  var step = 1 / (width / x2);
 
   //recorro todo el canvas
   for(var x = 0 ; x < width ; x++){
     for(var y = 0 ; y < height ; y++){
 
       //relacion los puntos del canvas con el punto a calcular
-      //var xi = (x - x1) / width;
-      //var yi = (y - y1) / height;
 
-      var xi = x * step;
-      var yi = y * step;
+      // var xi = (x - x1) * zoom;
+      // var yi = (y - y1) * zoom;
 
-      var mandelbrot_point = calculateSet(xi, yi, x2, y2, ratio, zoom);
+      //var xi = (x - x1) * ratio + x1; //funciona
+      //var yi = (y - y1) * ratio + y1; //funciona
+
+      var xi = x1 - (x1 - x) * ratio;
+      var yi = y1 - (y1 - y) * ratio;
+
+      //var xi = x * scale - x1;
+      //var yi = y * scale - y1;
+
+      var mandelbrot_point = calculateSet(xi, yi, width, height);
 
       if(mandelbrot_point == MAX_ITER) {
           ctx.fillStyle = '#000';
-          ctx.fillRect(x,y, 1,1); // Draw a black pixel
+          ctx.fillRect(x,y, 1, 1); // Draw a black pixel
       } else {
           ctx.fillStyle = 'hsl(0, 100%, ' + mandelbrot_point + '%)';
-          ctx.fillRect(x,y, 1,1); // Draw a colorful pixel
+          ctx.fillRect(x,y, 1, 1); // Draw a colorful pixel
       }
 
     }
   }
-}
 
-setInterval(draw,100);
+}
 
 // Mandelbrot set we need to get de imaginary part of C and the real part.
 // As we know, if |Zn| > 2 then the set converge, so we need to iterate between -2 and 2
-function calculateSet(x0,y0,x,y, ratio, zoom){
+function calculateSet(x0,y0,x,y){
 
-  var a = (-2 + (x0 / x) * (1 + 2)) * zoom * ratio;
-  var b = (-1 + (y0 / y) * (1 + 1)) * zoom * ratio;
+  var a = (-2 + (x0 / x) * (1 + 2));
+  var b = (-1 + (y0 / y) * (1 + 1));
 
   var ca = a;
   var cb = b;
@@ -82,11 +84,16 @@ function calculateSet(x0,y0,x,y, ratio, zoom){
 onmouseup = function(e) {
     div.hidden = 1;
 
+
     //Tomo la posicion del puntero cuando clickea
     x1 = e.clientX;
     y1 = e.clientY;
 
-    zoom *= 0.5;
+
+    //x2 = x2 / 2;
+    //y2 = y2 / 2;
+
+    ratio /= 2;
 
     draw();
 };
